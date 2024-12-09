375e62df1b2203a0cdb5aa85697918a4593278e3
Hasan <mh.mahmud.me@gmail.com>
2024-12-09 18:54:05 +0600

    lala

--- kb_patch.sql
+++ kb_patch.sql
@@ -239,3 +239,16 @@ CREATE TABLE `bloggers_category` (
   `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+
+CREATE TABLE IF NOT EXISTS `blogs` (
+  `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT,
+  `blog_category_id` int DEFAULT NULL,
+  `blog_name` varchar(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `blog_description` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
+  `blog_image` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `status` tinyint NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
