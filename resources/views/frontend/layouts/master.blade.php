8f67432aa750251ce24fa42cb7070b1734138949
rokibuzzaman <sourovh207@gmail.com>
2025-06-25 02:49:34 +0600

    index page change slider add

--- resources/views/frontend/layouts/master.blade.php
+++ resources/views/frontend/layouts/master.blade.php
@@ -45,7 +45,7 @@
                         <div class="row">
                             <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
                                 <div class="header-top-left">
-                                    <!--Start-Header-language-->
+                                    <!--Start-Header-language dd-->
                                     <div class="dropdown top-language-wrap"> <a role="button" data-toggle="dropdown" data-target="#" class="top-language dropdown-toggle" href="#"> <img src="{{url('/')}}/assets/frontend/images/flag/e.png" alt="language"> English <span class="caret"></span> </a>
                                         
                                         <ul class="dropdown-menu" role="menu">
